3b8019d8b68136f91bb281c09deca0a3d9a59868
Hanna Lata <annalats32@gmail.com>
2021-06-20 12:47:48 +0300

    Override keyPressed method

--- src/com/hannalata/GamePanel.java
+++ src/com/hannalata/GamePanel.java
@@ -99,6 +99,28 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkCollisions() {
+        // checks if head collides with body
+        for (int i = bodyParts; i > 0 ; i--) {
+            if(x[0] == x[i] && y[0] == y[i]) {
+                running = false;
+            }
+        }
+        //check if head touches left border
+        if(x[0] < 0) {
+            running = false;
+        }
+        //check if head touches right border
+        if(x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        //check if head touches top border
+        if(y[0] < 0) {
+            running = false;
+        }
+        //check if head touches bottom border
+        if(y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
 
     }
 
@@ -121,7 +143,26 @@ public class GamePanel extends JPanel implements ActionListener {
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-
+            switch (e.getKeyCode()) {
+                case  KeyEvent.VK_LEFT:
+                    if(direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case  KeyEvent.VK_RIGHT:
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case  KeyEvent.VK_UP:
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                case  KeyEvent.VK_DOWN:
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+            }
         }
     }
  }
